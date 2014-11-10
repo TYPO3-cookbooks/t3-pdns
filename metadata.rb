7aff686016e77a4130e8892c9a4639b07ec91afa
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-10 19:25:02 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache2"
 description      "Installs/Configures t3-pdns"
 long_description "Installs/Configures t3-pdns"
-version          "0.1.5"
+version          "0.1.6"
 
 depends          "pdns", "~> 0.3.4"
