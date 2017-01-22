d9e2e4059f5c618405e95082cc2d19bd2c4d9f8a
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-22 01:10:02 +0100

    [TASK] Give depsolver a hint

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures t3-pdns"
 long_description "Installs/Configures t3-pdns"
 version          "1.1.0"
 
-depends          "t3-base", "~> 0.2.0"
+depends          "t3-base", "~> 0.2.59"
 
 depends          "zabbix-custom-checks", "~> 0.2.6"
 
