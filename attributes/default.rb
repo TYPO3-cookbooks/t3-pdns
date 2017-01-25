1e39a0a2da9aa03683cac19275692f905f8297bd
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-25 11:09:10 +0100

    [BUGFIX] Turn on master functionality

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,8 @@ default['pdns']['authoritative']['config']['allow_recursion'] = '127.0.0.1'
 #<> Just a test value
 default['pdns']['authoritative']['config']['allow_axfr_ips'] = ['192.168.1.1']
 default['pdns']['authoritative']['config']['disable_axfr'] = false
+#<> Turn on master functionality
+default['pdns']['authoritative']['config']['disable_axfr'] = false
 
 # default['pdns']['authoritative']['config']['local_ipv6'] = "::1 #{node[:ip6address]}"
 default['pdns']['authoritative']['config']['local_ipv6'] = "::"
