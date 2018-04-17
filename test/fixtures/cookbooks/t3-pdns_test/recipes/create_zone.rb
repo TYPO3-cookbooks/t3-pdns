b0a9a9dfd61b47dfcb30644c05117fd00ce5830e
Steffen Gebert <steffen.gebert@typo3.org>
2018-04-18 00:27:09 +0200

    [TESTS] Fix example zone to pass named-checkzone

--- test/fixtures/cookbooks/t3-pdns_test/recipes/create_zone.rb
+++ test/fixtures/cookbooks/t3-pdns_test/recipes/create_zone.rb
@@ -4,10 +4,12 @@ t3_pdns_zone "example.com" do
   soa            "ns"
   contact        "admin.example.com"
   global_ttl     300
-  mail_exchange  [{:host => "mail.example.com", :priority => 20}]
-  nameserver     ["a.com"]
+  mail_exchange  [{:host => "mail.example.com.", :priority => 20}]
+  nameserver     ["ns.example.org."]
   records        ([
-      {:name => "@",   :type => "A",     :value => "1.2.3.4"},
-      {:name => "www", :type => "CNAME", :value => "example.org."},
+      {:name => "@",    :type => "A",     :value => "192.0.2.1"},
+      {:name => "www",  :type => "CNAME", :value => "example.org."},
+      {:name => "mail", :type => "A",     :value => "192.0.2.2"},
+      {:name => "ns",   :type => "A",     :value => "192.0.2.3"},
     ])
 end
